de141b117bb9cc0fda3fb72e7d4459de0b14b4f8
Leedabord <gwforumlakers@gmail.com>
2022-07-03 17:11:55 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -67,7 +67,32 @@ export class AppComponent  implements OnInit, OnDestroy {
     { name: 'Warn', color: 'warn' }
   ];
 
-  constructor(private _dialog: MatDialog, private _snackbar: MatSnackBar, private session: SessionService) {
+  public Teams = [ 
+    {tag: "LIV", wins: 0, bonus: 0, skins: 0, tsf9: 0, tsb9: 0, tsr18: 0, 
+    hw: [false,false,false,false,false,false,false,false,false,
+        false,false,false,false,false,false,false,false,false],
+        sf9: [8,8,8,8,8,8,8,8,8],
+        sb9: [8,8,8,8,8,8,8,8,8] },
+    {tag: "PGA", wins: 0, bonus: 0, skins: 0, tsf9: 0, tsb9: 0, tsr18: 0, 
+    hw: [false,false,false,false,false,false,false,false,false,
+        false,false,false,false,false,false,false,false,false],
+        sf9: [8,8,8,8,8,8,8,8,8],
+        sb9: [8,8,8,8,8,8,8,8,8] },
+    {tag: "Buggers", wins: 0, bonus: 0, skins: 0, tsf9: 0, tsb9: 0, tsr18: 0, 
+    hw: [false,false,false,false,false,false,false,false,false,
+        false,false,false,false,false,false,false,false,false],
+        sf9: [8,8,8,8,8,8,8,8,8],
+        sb9: [8,8,8,8,8,8,8,8,8] },
+    {tag: "Mango", wins: 0, bonus: 0, skins: 0, tsf9: 0, tsb9: 0, tsr18: 0, 
+    hw: [false,false,false,false,false,false,false,false,false,
+        false,false,false,false,false,false,false,false,false],
+        sf9: [8,8,8,8,8,8,8,8,8],
+        sb9: [8,8,8,8,8,8,8,8,8] },
+  ];
+
+  constructor(private _dialog: MatDialog, private _snackbar: MatSnackBar, 
+//    private httpC: HttpClient, 
+    private session: SessionService) {
     this.currentUser = session.userName;
 
     // Update the value for the progress-bar on an interval.
@@ -105,6 +130,60 @@ export class AppComponent  implements OnInit, OnDestroy {
   set tickInterval(v) {
     this.slider.tickInterval = Number(v);
   }
+
+  tempfn() { 
+    let skinpot = 0; let sf9 = 0;
+  
+    for (let ii = 0; ii < 9; ii++) {
+      skinpot += 1;
+      let minscore = Math.min(this.Teams[0].sf9[ii], this.Teams[1].sf9[ii],
+        this.Teams[2].sf9[ii], this.Teams[3].sf9[ii] );  
+      let winner = 9;
+      for (let jj = 0; jj < 4; jj++) {
+        this.Teams[jj].hw[ii] = false;
+        if (this.Teams[jj].sf9[ii] == minscore) {
+          if (winner == 9) {
+            winner = jj;  
+          } else {
+            winner = 9;
+          }
+        }
+      }
+      if (winner < 4) {
+        this.Teams[winner].hw[ii] = true;
+        this.Teams[winner].skins += skinpot;
+        skinpot = 0;
+      }
+    }
+  
+    for (let ii = 0; ii < 9; ii++) {
+      skinpot += 1;
+      let minscore = Math.min(this.Teams[0].sb9[ii], this.Teams[1].sb9[ii],
+        this.Teams[2].sb9[ii], this.Teams[3].sb9[ii] );  
+      let winner = 9;
+      for (let jj = 0; jj < 4; jj++) {
+        this.Teams[jj].hw[ii] = false;
+        if (this.Teams[jj].sb9[ii] == minscore) {
+          if (winner == 9) {
+            winner = jj;  
+          } else {
+            winner = 9;
+          }
+        }
+      }
+      if (winner < 4) {
+        this.Teams[winner].hw[ii] = true;
+        this.Teams[winner].skins += skinpot;
+        skinpot = 0;
+      }
+    }
+  
+    for (let jj = 0; jj < 4; jj++) {
+      this.Teams[jj].wins = this.Teams[jj].bonus + this.Teams[jj].skins;
+    }
+  
+  } // end tempfn()
+
 }
 
 
