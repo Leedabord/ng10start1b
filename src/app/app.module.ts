cfda846aaa2780879d64ed59e9479bc9ecc6a4b5
Leedabord <gwforumlakers@gmail.com>
2022-07-04 12:32:04 -0700

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import 'hammerjs';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppMaterialModule } from './app.material.module';
+import { MatInputModule } from '@angular/material/input';
 import { AppComponent, DialogContentComponent } from './app.component';
 import { SessionService } from './services/session.service';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HelloComponent } from './hello.component';
-// import { MatInputModule } from '@angular/material/input';
 import { FirstDemoComponent } from './first-demo/first-demo.component'
 
 @NgModule({
@@ -23,11 +27,26 @@ import { FirstDemoComponent } from './first-demo/first-demo.component'
     // MatInputModule,   
     AppMaterialModule,    
     HttpClientModule,   
+    RouterModule.forRoot([
+      {path: 'crisis-list', component: CrisisListComponent},
+      {path: 'heroes-list', component: HeroesListComponent},
+      {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
   ],
-  declarations: [AppComponent, DialogContentComponent, 
-    HelloComponent, FirstDemoComponent ],
-  entryComponents: [DialogContentComponent],
+  declarations: [AppComponent, // DialogContentComponent, 
+    HelloComponent, FirstDemoComponent,
+    CrisisListComponent,
+    HeroesListComponent,
+    PageNotFoundComponent
+  ],
+//  entryComponents: [DialogContentComponent],
   providers: [SessionService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
