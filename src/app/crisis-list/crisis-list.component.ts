38371a67261ce6062778e3a2d7121b5ca5cdb9b1
Leedabord <gwforumlakers@gmail.com>
2022-07-05 10:40:07 -0700

    3 files modified

--- src/app/crisis-list/crisis-list.component.ts
+++ src/app/crisis-list/crisis-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-crisis-list',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./crisis-list.component.css']
 })
 export class CrisisListComponent {
-
+  name = 'Angular ' + VERSION.major;
 }
 
 
